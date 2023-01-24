db6a08f35a19b49858c2213e70f219843cb3974b
KKocot <k.kocot0@gmail.com>
2023-01-24 17:26:12 +0100

    button-first-draw

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,1 +1,34 @@
-export const Button = () => <button>Kliknij mnie</button>;
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+interface PropsButton {
+  className?: string;
+  children?: ReactNode;
+  onClick: () => void;
+  variant?: "primary" | "secondary" | "warning";
+  size?: "sm" | "md";
+}
+
+export const Button = ({
+  className,
+  children,
+  onClick,
+  variant = "primary",
+  size = "sm",
+}: PropsButton) => (
+  <button
+    onClick={onClick}
+    className={clsx(
+      {
+        "bg-lime-700  ": variant === "primary",
+        "bg-red-900 ": variant === "secondary",
+        "bg-red-600 ": variant === "warning",
+      },
+      { "px-2 py-1": size === "sm", "px-4 py-2": size === "md" },
+      " m-1 rounded text-gray-50",
+      className
+    )}
+  >
+    {children}
+  </button>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,20 @@
+import { Button } from "../components/button";
+
 export default function Home() {
-  return <div>tutaj bedzie logowanko</div>;
+  return (
+    <div>
+      <div>tutaj bedzie logowanko</div>
+      <Button onClick={() => alert("Dodany")}>Dodaj</Button>
+      <Button
+        onClick={() => alert("Wypożyczony")}
+        variant="secondary"
+        size="md"
+      >
+        Wypożycz
+      </Button>
+      <Button onClick={() => alert("Usunięty")} variant="warning">
+        Usuć
+      </Button>
+    </div>
+  );
 }
